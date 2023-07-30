30e6b76f83cae44758832544fac20641554e970b
P.Sergey <pavbatol@yandex.ru>
2023-07-30 12:24:21 +0400

    fix:stats: Fix finding CartItem with 'unique' in filter

--- mp-stats/dto/src/main/java/ru/pavbatol/myplace/dto/cart/CartItemDtoResponse.java
+++ mp-stats/dto/src/main/java/ru/pavbatol/myplace/dto/cart/CartItemDtoResponse.java
@@ -1,21 +1,13 @@
 package ru.pavbatol.myplace.dto.cart;
 
-import lombok.*;
+import lombok.Builder;
+import lombok.Value;
 import lombok.extern.jackson.Jacksonized;
-
-import java.rmi.server.UID;
 
 @Value
 @Builder
 @Jacksonized
-
-//@Value
-//@Builder
-//@NoArgsConstructor(force = true)
-//@AllArgsConstructor
 public class CartItemDtoResponse {
-//    @With
     Long itemId;
-//    @With
     int cartItemCount;
 }
--- mp-stats/server/src/main/java/ru/pavbatol/myplace/stats/cart/repository/CustomCartItemMongoRepositoryImpl.java
+++ mp-stats/server/src/main/java/ru/pavbatol/myplace/stats/cart/repository/CustomCartItemMongoRepositoryImpl.java
@@ -18,6 +18,7 @@ public class CustomCartItemMongoRepositoryImpl implements CustomCartItemMongoRep
     public static final String CART_ITEM_COUNT = "cartItemCount";
     public static final String USER_ID = "userId";
     public static final String CART_ITEMS = "cartItems";
+    public static final String TEMP = "temp";
     private final ReactiveMongoTemplate reactiveMongoTemplate;
 
     @Override
@@ -26,30 +27,16 @@ public class CustomCartItemMongoRepositoryImpl implements CustomCartItemMongoRep
                 ? Sort.Direction.ASC : Sort.Direction.DESC;
 
         MatchOperation betweenDates = Aggregation.match(new Criteria(TIMESTAMP).gte(filter.getStart()).lte(filter.getEnd()));
-
-//        MatchOperation inItemIds = Aggregation.match(new Criteria(ITEM_ID).in(filter.getItemIds()));
         MatchOperation inItemIds = Aggregation.match(CollectionUtils.isEmpty(filter.getItemIds())
                 ? new Criteria() : new Criteria(ITEM_ID).in(filter.getItemIds()));
-
-//        GroupOperation group = Aggregation.group(ITEM_ID, USER_ID);
-        GroupOperation group = Aggregation.group(ITEM_ID, USER_ID, CART_ITEM_COUNT);
-//        GroupOperation groupAndCount = Aggregation.group(ITEM_ID, USER_ID).count().as(CART_ITEM_COUNT);
-        GroupOperation groupAndCount = Aggregation.group(ITEM_ID, CART_ITEM_COUNT).count().as(CART_ITEM_COUNT);
-
+        GroupOperation group = Aggregation.group(ITEM_ID, USER_ID, "-");
+        GroupOperation groupAndCount = Aggregation.group(ITEM_ID, "-").addToSet(USER_ID).as(TEMP).count().as(CART_ITEM_COUNT);
         SortOperation sort = Aggregation.sort(direction, CART_ITEM_COUNT);
         ProjectionOperation projection = Aggregation.project(ITEM_ID, CART_ITEM_COUNT);
 
-        // TODO: 28.07.2023 Do case for empty collection
         Aggregation aggregation = filter.getUnique()
                 ? Aggregation.newAggregation(betweenDates, inItemIds, group, groupAndCount, sort, projection)
                 : Aggregation.newAggregation(betweenDates, inItemIds, groupAndCount, sort, projection);
-
-//        aggregation = Aggregation.newAggregation(betweenDates, inItemIds, groupAndCount, sort, projection);
-//        aggregation = Aggregation.newAggregation(group, groupAndCount, sort, projection);
-//        aggregation = Aggregation.newAggregation(
-//                group,
-//                Aggregation.group(ITEM_ID, USER_ID).count().as(CART_ITEM_COUNT),
-//                Aggregation.project(ITEM_ID, CART_ITEM_COUNT));
 
         return reactiveMongoTemplate.aggregate(aggregation, CART_ITEMS, CartItemDtoResponse.class);
     }
