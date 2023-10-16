b1f88048627a350d99391cd0cf41a749e5e93428
P.Sergey <pavbatol@yandex.ru>
2023-10-16 14:33:27 +0400

    fix:security: Replace .keys with .scan to get keys from Redis

--- mp-security/src/main/java/ru/pavbatol/myplace/auth/repository/RefreshTokenRedisRepositoryImpl.java
+++ mp-security/src/main/java/ru/pavbatol/myplace/auth/repository/RefreshTokenRedisRepositoryImpl.java
@@ -3,11 +3,16 @@ package ru.pavbatol.myplace.auth.repository;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
+import org.springframework.data.redis.core.Cursor;
+import org.springframework.data.redis.core.ScanOptions;
+import org.springframework.data.redis.serializer.RedisSerializer;
 import org.springframework.stereotype.Component;
+import ru.pavbatol.myplace.app.exception.RedisException;
 import ru.pavbatol.myplace.app.redis.RedisKey;
 import ru.pavbatol.myplace.app.redis.repository.AbstractRedisRepository;
 
 import javax.validation.constraints.NotNull;
+import java.util.HashSet;
 import java.util.Set;
 
 @Slf4j
@@ -27,15 +32,38 @@ public class RefreshTokenRedisRepositoryImpl extends AbstractRedisRepository<Str
     }
 
     @Override
-    public void deleteAllByKeyPattern(@NotNull String keyStartWith) {
+    public void deleteAllByKeyStartWith(@NotNull String keyStartWith) {
         String pattern = composeKey(keyStartWith) + "*";
-        Set<String> keys = redisTemplate.keys(pattern);
-        if (keys != null && !keys.isEmpty()) {
+        Set<String> keys = scanForKeys(pattern);
+        if (!keys.isEmpty()) {
             log.debug("Found {} keys to delete by keyStartWith: {}", keys.size(), keyStartWith);
             redisTemplate.delete(keys);
             log.debug("Deleted all for keyStartWith {}", keyStartWith);
         } else {
             log.debug("Not found keys to delete by keyStartWith: {}", keyStartWith);
         }
+    }
+
+    @NotNull
+    private Set<String> scanForKeys(String pattern) {
+        int count = 1000;
+        Set<String> keys = new HashSet<>();
+        RedisSerializer<?> serializer = redisTemplate.getKeySerializer();
+        ScanOptions scanOptions = ScanOptions.scanOptions().match(pattern).count(count).build();
+
+        try (Cursor<byte[]> cursor = redisTemplate.execute(connection -> connection.scan(scanOptions), true);) {
+            if (cursor == null) {
+                throw new RedisException("Failed obtaining Cursor<byte[]>");
+            }
+            while (cursor.hasNext()) {
+                keys.add(String.valueOf(serializer.deserialize(cursor.next())));
+            }
+        } catch (Exception e) {
+            String message = e.getMessage() != null ? e.getMessage() : "no massage";
+            log.error(message, e);
+            throw new RedisException("Scanning error", message);
+        }
+
+        return keys;
     }
 }
