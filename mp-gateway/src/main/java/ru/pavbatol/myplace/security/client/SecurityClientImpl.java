6ddc3a797902731b69bb61047ab3127bed608c04
pavbatol <pavbatol@yandex.ru>
2025-03-14 14:13:46 +0400

    refactor:(gateway) Optimize SecurityClientImpl.

--- mp-gateway/src/main/java/ru/pavbatol/myplace/security/client/SecurityClientImpl.java
+++ mp-gateway/src/main/java/ru/pavbatol/myplace/security/client/SecurityClientImpl.java
@@ -30,7 +30,9 @@ public class SecurityClientImpl extends BaseRestClient implements SecurityClient
     private static final String PUBLIC_AUTH_CONTEXT = "/auth";
     private final ResponseHandler responseHandler;
 
-    public SecurityClientImpl(@Value("${app.mp.security.url}") String serverUrl, RestTemplateBuilder builder, ResponseHandler responseHandler) {
+    public SecurityClientImpl(@Value("${app.mp.security.url}") String serverUrl,
+                              RestTemplateBuilder builder,
+                              ResponseHandler responseHandler) {
         super(builder.uriTemplateHandler(new DefaultUriBuilderFactory(serverUrl))
                 .requestFactory(HttpComponentsClientHttpRequestFactory::new)
                 .build());
@@ -44,7 +46,6 @@ public class SecurityClientImpl extends BaseRestClient implements SecurityClient
         String fullResourcePath = ADMIN_AUTH_CONTEXT + resourcePath;
 
         ResponseEntity<Object> response = delete(fullResourcePath);
-
         ApiResponse<Void> apiResponse = responseHandler.processResponse(response, Void.class);
 
         return ResponseEntity.status(response.getStatusCode()).body(apiResponse);
@@ -56,7 +57,6 @@ public class SecurityClientImpl extends BaseRestClient implements SecurityClient
         String fullResourcePath = ADMIN_AUTH_CONTEXT + resourcePath;
 
         ResponseEntity<Object> response = delete(fullResourcePath);
-
         ApiResponse<Void> apiResponse = responseHandler.processResponse(response, Void.class);
 
         return ResponseEntity.status(response.getStatusCode()).body(apiResponse);
@@ -68,126 +68,89 @@ public class SecurityClientImpl extends BaseRestClient implements SecurityClient
         String fullResourcePath = ADMIN_AUTH_CONTEXT + resourcePath;
 
         ResponseEntity<Object> response = delete(fullResourcePath);
-
         ApiResponse<Void> apiResponse = responseHandler.processResponse(response, Void.class);
 
         return ResponseEntity.status(response.getStatusCode()).body(apiResponse);
     }
 
     @Override
-    public ResponseEntity<ApiResponse<AuthDtoResponse>> getNewRefreshToken(HttpServletRequest request, AuthDtoRefreshRequest dtoRefreshRequest) {
+    public ResponseEntity<ApiResponse<AuthDtoResponse>> getNewRefreshToken(HttpServletRequest httpServletRequest,
+                                                                           AuthDtoRefreshRequest dtoRefreshRequest) {
         String resourcePath = "/refresh-tokens";
         String fullResourcePath = PRIVATE_AUTH_CONTEXT + resourcePath;
 
-        ResponseEntity<Object> response;
-        try {
-            response = rest.exchange(
-                    fullResourcePath,
-                    HttpMethod.POST,
-                    new HttpEntity<>(dtoRefreshRequest, extractHeaders(request)),
-                    Object.class
-            );
-        } catch (RestClientException e) {
-            throw new RuntimeException("Failed to call target service: " + e.getMessage(), e);
-        }
-
+        ResponseEntity<Object> response = executeRequest(fullResourcePath, HttpMethod.POST, dtoRefreshRequest, httpServletRequest);
         ApiResponse<AuthDtoResponse> apiResponse = responseHandler.processResponse(response, AuthDtoResponse.class);
 
         return ResponseEntity.status(response.getStatusCode()).body(apiResponse);
     }
 
     @Override
-    public ResponseEntity<ApiResponse<String>> logoutAllSessions(HttpServletRequest request) {
+    public ResponseEntity<ApiResponse<String>> logoutAllSessions(HttpServletRequest httpServletRequest) {
         String resourcePath = "/logout/all";
         String fullResourcePath = PRIVATE_AUTH_CONTEXT + resourcePath;
 
-        ResponseEntity<Object> response;
-        try {
-            response = rest.exchange(
-                    fullResourcePath,
-                    HttpMethod.POST,
-                    new HttpEntity<>(null, extractHeaders(request)),
-                    Object.class
-            );
-        } catch (RestClientException e) {
-            throw new RuntimeException("Failed to call target service: " + e.getMessage(), e);
-        }
-
+        ResponseEntity<Object> response = executeRequest(fullResourcePath, HttpMethod.POST, null, httpServletRequest);
         ApiResponse<String> apiResponse = responseHandler.processResponse(response, String.class);
 
         return ResponseEntity.status(response.getStatusCode()).body(apiResponse);
     }
 
     @Override
-    public ResponseEntity<ApiResponse<String>> logout(HttpServletRequest request) {
+    public ResponseEntity<ApiResponse<String>> logout(HttpServletRequest httpServletRequest) {
         String resourcePath = "/logout";
         String fullResourcePath = PUBLIC_AUTH_CONTEXT + resourcePath;
 
-        ResponseEntity<Object> response;
-        try {
-            response = rest.exchange(
-                    fullResourcePath,
-                    HttpMethod.POST,
-                    new HttpEntity<>(null, extractHeaders(request)),
-                    Object.class
-            );
-        } catch (RestClientException e) {
-            throw new RuntimeException("Failed to call target service: " + e.getMessage(), e);
-        }
-
+        ResponseEntity<Object> response = executeRequest(fullResourcePath, HttpMethod.POST, null, httpServletRequest);
         ApiResponse<String> apiResponse = responseHandler.processResponse(response, String.class);
 
         return ResponseEntity.status(response.getStatusCode()).body(apiResponse);
     }
 
     @Override
-    public ResponseEntity<ApiResponse<AuthDtoResponse>> login(HttpServletRequest request, AuthDtoRequest dtoAuthRequest) {
+    public ResponseEntity<ApiResponse<AuthDtoResponse>> login(HttpServletRequest httpServletRequest, AuthDtoRequest dtoAuthRequest) {
         String resourcePath = "/login";
         String fullResourcePath = PUBLIC_AUTH_CONTEXT + resourcePath;
 
-        ResponseEntity<Object> response;
-        try {
-            response = rest.exchange(
-                    fullResourcePath,
-                    HttpMethod.POST,
-                    new HttpEntity<>(dtoAuthRequest, extractHeaders(request)),
-                    Object.class
-            );
-        } catch (RestClientException e) {
-            throw new RuntimeException("Failed to call target service: " + e.getMessage(), e);
-        }
-
+        ResponseEntity<Object> response = executeRequest(fullResourcePath, HttpMethod.POST, dtoAuthRequest, httpServletRequest);
         ApiResponse<AuthDtoResponse> apiResponse = responseHandler.processResponse(response, AuthDtoResponse.class);
 
         return ResponseEntity.status(response.getStatusCode()).body(apiResponse);
     }
 
     @Override
-    public ResponseEntity<ApiResponse<AuthDtoResponse>> getNewAccessToken(HttpServletRequest request, AuthDtoRefreshRequest dtoRefreshRequest) {
+    public ResponseEntity<ApiResponse<AuthDtoResponse>> getNewAccessToken(HttpServletRequest httpServletRequest,
+                                                                          AuthDtoRefreshRequest dtoRefreshRequest) {
         String resourcePath = "/tokens";
         String fullResourcePath = PUBLIC_AUTH_CONTEXT + resourcePath;
 
-        ResponseEntity<Object> response;
-        try {
-            response = rest.exchange(
-                    fullResourcePath,
-                    HttpMethod.POST,
-                    new HttpEntity<>(dtoRefreshRequest, extractHeaders(request)),
-                    Object.class
-            );
-        } catch (RestClientException e) {
-            throw new RuntimeException("Failed to call target service: " + e.getMessage(), e);
-        }
-
+        ResponseEntity<Object> response = executeRequest(fullResourcePath, HttpMethod.POST, dtoRefreshRequest, httpServletRequest);
         ApiResponse<AuthDtoResponse> apiResponse = responseHandler.processResponse(response, AuthDtoResponse.class);
 
         return ResponseEntity.status(response.getStatusCode()).body(apiResponse);
     }
 
-    private HttpHeaders extractHeaders(HttpServletRequest request) {
+    private HttpHeaders extractHeaders(HttpServletRequest httpServletRequest) {
         HttpHeaders headers = new HttpHeaders();
-        Collections.list(request.getHeaderNames())
-                .forEach(headerName -> headers.set(headerName, request.getHeader(headerName)));
+        Collections.list(httpServletRequest.getHeaderNames())
+                .forEach(headerName -> headers.set(headerName, httpServletRequest.getHeader(headerName)));
         return headers;
+    }
+
+
+    private <T> ResponseEntity<Object> executeRequest(String fullResourcePath,
+                                                      HttpMethod httpMethod,
+                                                      T body,
+                                                      HttpServletRequest httpServletRequest) {
+        try {
+            return rest.exchange(
+                    fullResourcePath,
+                    httpMethod,
+                    new HttpEntity<>(body, extractHeaders(httpServletRequest)),
+                    Object.class
+            );
+        } catch (RestClientException e) {
+            throw new RuntimeException("Failed to call target service: " + e.getMessage(), e);
+        }
     }
 }
