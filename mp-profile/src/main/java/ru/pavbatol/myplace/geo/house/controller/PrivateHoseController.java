0e84459568bd7cd922c82375648448eb25a783a4
pavbatol <pavbatol@yandex.ru>
2025-05-31 18:15:27 +0400

    fix(profile): CodeStyle
    - Removed unused import in PrivateHoseController

--- mp-profile/src/main/java/ru/pavbatol/myplace/geo/house/controller/PrivateHoseController.java
+++ mp-profile/src/main/java/ru/pavbatol/myplace/geo/house/controller/PrivateHoseController.java
@@ -4,7 +4,6 @@ import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.data.domain.Slice;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import ru.pavbatol.myplace.geo.house.dto.HouseDto;
