37a80ae81641b3b7380b1a99c74ee50304ed058f
pavbatol <pavbatol@yandex.ru>
2025-08-05 20:52:43 +0400

    fix(security): Code style
    Contributes to 'profile' functionality

--- mp-security/src/main/java/ru/pavbatol/myplace/security/permission/controller/PermissionController.java
+++ mp-security/src/main/java/ru/pavbatol/myplace/security/permission/controller/PermissionController.java
@@ -6,7 +6,6 @@ import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.validation.annotation.Validated;
